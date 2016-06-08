3622441d67354cb85ddec506e2c49f07e7a5433d
Uenes Vilaça <ueneslucio@gmail.com>
2016-06-08 01:09:37 -0300

    Design melhorado para funcionar em mobile

--- index.php
+++ index.php
@@ -10,31 +10,8 @@
         <link href="css/index.css" rel="stylesheet">
     </head>
     <body>
-        <nav class="navbar navbar-default navbar-fixed-top">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
 
-                    <a class="navbar-brand" href="#topSection">
-                        <img alt="Brand" src="images/iconCharod32x32.png" >
-                    </a>
-                </div>
-
-                <!-- Collect the nav links, forms, and other content for toggling -->
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul class="nav navbar-nav">
-                        <li><a href="#sobreNosSection">Sobre Nós</a></li>
-                        <li><a href="#identidadeCidada">Identidade Cidadã</a></li>
-                        <li><a href="#contato">Contato</a></li>
-                    </ul>
-                </div><!-- /.navbar-collapse -->
-            </div>
-        </nav>
+        <?php include("templates/bar.html") ?>
 
         <div class="bannerSection" > 
             <div class="jumbotron" id="topSection">
@@ -52,14 +29,13 @@
 
         <section id="sobreNosSection" >
             <div class="darkSection">
-                <h1> Sobre Nós </h1>
-                <h2>Missão</h2>
+                <h2><b>MISSÃO</b></h2>
                 <h3>Proporcionar maior conforto para as pessoas através da tecnologia com a geração de valores para nossos clientes e os stakeholders.</h3>
                 </br></br>
-                <h2>Visão</h2>
+                <h2><b>VISÃO</b></h2>
                 <h3>Oferecer um serviço de excelência e inovação para empresas e governos.</h3>
                 </br></br>
-                <h2>Valores</h2>
+                <h2><b>VALORES</b></h2>
                 <h3>Conquistar a <b>confiança</b> dos clientes </br>
                     <b>Comprometimento</b> com os resultados diante de clientes e stakeholders </br>
                     Oferecer <b>transparência</b> através de comunicação clara e contínuos feedbacks</br>
@@ -68,15 +44,15 @@
         </section>
 
         <div class="bannerSectionCidada" id="identidadeCidada"> 
-            
-                <div class="container">
-                    <div class="ca_zone ca_zone1">
-                        <div class="ca_wrap ca_wrap1">
-                            <img class="bigbanner" id="cidadeCidada" src="images/bannerIdentidadeCidada.png" >
-                        </div>
+
+            <div class="container">
+                <div class="ca_zone ca_zone1">
+                    <div class="ca_wrap ca_wrap1">
+                        <img class="bigbanner" id="cidadeCidada" src="images/bannerIdentidadeCidada.png" >
                     </div>
                 </div>
-            
+            </div>
+
         </div>
 
         <?php
@@ -111,11 +87,11 @@
                 <label class="col-lg-2 control-label" > Nome </label>
                 <div class="col-lg-6">
                     <input name="name" type="text" class="form-control input-sm" value="<?php echo $name; ?>" />
-                </div></br></br></br>
+                </div></br></br>
                 <label for="inputEmail" class="col-lg-2 control-label" > Email </label>
                 <div class="col-lg-4">
                     <input name="email" type="text" class="form-control input-sm" value="<?php echo $email; ?>"/>
-                </div></br></br></br>
+                </div></br></br>
                 <label for="inputEmail" class="col-lg-2 control-label" > Motivo do Contato </label>
                 <div class="col-lg-4">
                     <select class="form-control" name="assunto" >
@@ -130,7 +106,7 @@
                 <div class="col-lg-10">
                     <textarea name="message" rows="3" class="form-control input-sm" value="<?php echo $message; ?>"></textarea>
                 </div>
-                </br></br></br></br></br>
+                </br></br></br></br>
                 <button type="submit" name="submit" class="btn btn-primary btn-lg" >Enviar </button>
             </form>
         </section>
@@ -140,7 +116,6 @@
                 <div class="col-1g-12">
                     <p>Made by Uenes Ferreira. Contact him at ueneslucio@gmail.com </p>
                     <p>Charod Soluções</p>
-                    <p>Entre em contato: charod@charod.com.br</p>
                 </div>
             </footer>
         </div>
